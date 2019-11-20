a6639eba36594bd8fba8689ce1eadf888b605151
Schouten <607953-l0119@slim365.nl>
2019-11-20 11:24:46 +0100

    Komt er nu een conflict?
    Hoop het wel.

--- GithubTest/Informatica/Program.cs
+++ GithubTest/Informatica/Program.cs
@@ -8,7 +8,7 @@ namespace Informatica
         {
             Console.WriteLine("Hello World!");
             //yhfdrf
-            Console.WriteLine("test2");
+            Console.WriteLine("test4");
         }
     }
 }
