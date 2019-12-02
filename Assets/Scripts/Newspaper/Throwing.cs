146e2912bac7fd93e340f160f71ad296ec49a3c1
Rowin R. Schouten <boyrowin@gmail.com>
2019-12-02 13:32:50 +0100

    Throwing newspaper
    Het gooien van de krant naar links en het verwijnen van de krant tegen het raam aan.

---
+++ Assets/Scripts/Newspaper/Throwing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(-20 * GetComponent<Variables>().richting, 0, 0);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (GetComponent<Rigidbody>().velocity.x < 0)
+        {
+            GetComponent<Rigidbody>().velocity += new Vector3(0.1f, 0, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/ThrowNewspaper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowNewspaper : MonoBehaviour
+{
+    public GameObject newspaper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            Instantiate(newspaper, new Vector3(transform.position.x -1.2f, transform.position.y + 1.8F, transform.position.z), Quaternion.identity);
+            newspaper.GetComponent<Variables>().richting = 1;
+        }
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            Instantiate(newspaper, new Vector3(transform.position.x + 2f, transform.position.y + 1.8F, transform.position.z), Quaternion.identity);
+            newspaper.GetComponent<Variables>().richting = -1;
+        }
+    }
+}
