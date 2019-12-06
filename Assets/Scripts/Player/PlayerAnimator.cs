abbce6be1b9c7245b52798282a4ee325b223c4f3
Jarno Hendriksen <jarno.hendriksen@outlook.com>
2019-12-06 13:57:05 +0100

    Animatie toegevoegd

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        gameObject.GetComponent<Animator>().Play("Player Animation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer++;
+
+        if (timer % 45 == 0)
+        {
+            gameObject.GetComponent<Animator>().Play("Player Animation", -1, 0);
+        }
+    }
+}
