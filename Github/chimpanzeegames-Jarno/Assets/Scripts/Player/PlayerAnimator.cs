95e564a6d50a3e7c166c7f7d08747c6f1d762ff6
Rowin R. Schouten <boyrowin@gmail.com>
2019-12-08 13:14:29 +0100

    Verbeterde animaties
    - Armen bewegen bij gooien
    - Sturen
    - Krant in elke richting gooien

---
+++ Github/chimpanzeegames-Jarno/Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    int timer;
+
+    void Start()
+    {
+        timer = 0;
+        if (GetComponent<ControlledVelocity>().speed != 0) gameObject.GetComponent<Animator>().Play("Player Animation");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.GetComponent<Animator>().Play("Steering Left");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Animator>().Play("Steering Right", -1, 0f);
+        }
+        else if (Input.GetKey(KeyCode.N))
+        {
+            gameObject.GetComponent<Animator>().Play("Throwing Left");
+        }
+        else if (Input.GetKey(KeyCode.M))
+        {
+            gameObject.GetComponent<Animator>().Play("Throwing Right");
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().Play("Player Animation");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (GetComponent<ControlledVelocity>().speed != 0)
+        {
+            gameObject.GetComponent<Animator>().speed = GetComponent<ControlledVelocity>().speed / (GetComponent<ControlledVelocity>().maxSpeed / 2);
+            timer++;
+
+            if (timer % 210 == 0)
+            {
+                gameObject.GetComponent<Animator>().Play("Player Animation", -1, 0.05f);
+            }
+        }
+
+        else
+        {
+            gameObject.GetComponent<Animator>().speed = 0;
+        }
+    }
+}
