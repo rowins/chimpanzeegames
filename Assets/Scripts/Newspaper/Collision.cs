e5a3479ce40e9c37d8e831ca0666f2b8cee82637
Jarno Hendriksen <jarno.hendriksen@outlook.com>
2019-12-12 20:07:22 +0100

    Muur achter speler toegevoegd
    Als de afstand tussen de speler en de onzichtbare muur te groot wordt, wordt de muur achter de speler gezet, waardoor hij niet meer terug kan.

---
+++ Assets/Scripts/Newspaper/Collision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider otherObj)
+    {
+        if (otherObj.gameObject.tag == "window")
+        {
+            Destroy(this.gameObject);
+            Debug.Log(1);
+        }
+
+        if (otherObj.gameObject.tag == "bound")
+        {
+            Physics.IgnoreCollision(wall.GetComponent<BoxCollider>(), GetComponent<BoxCollider>());
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ControlledVelocity.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlledVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 Force;
+
+    public float speed;
+    public float maxSpeed;
+
+    [SerializeField]
+    float acceleration;
+
+    [SerializeField]
+    float turnSpeed;
+
+    [SerializeField]
+    KeyCode keyForward;
+
+    [SerializeField]
+    KeyCode keyBackward;
+
+    [SerializeField]
+    KeyCode keyLeft;
+
+    [SerializeField]
+    KeyCode keyRight;
+
+    public float angleY;
+    public float velX, velZ;
+    float initialTurnSpeed;
+    public Text textDBG;
+    public bool inGate;
+
+    void Awake()
+    {
+        speed = 0;
+        maxSpeed = 900;
+        acceleration = 100;
+        initialTurnSpeed = 100;
+        inGate = false;
+    }
+
+    void FixedUpdate()
+    {
+        CalculateSpeed();
+        turnSpeed = initialTurnSpeed + (speed * 0.01f);
+
+        if (Input.GetKey(keyForward))
+        {
+            if (speed <= maxSpeed) speed += acceleration * Time.deltaTime;
+        }
+
+        if (Input.GetKey(keyBackward))
+        {
+            if (speed >= 0) speed -= acceleration * 3 * Time.deltaTime;
+        }
+
+        if (speed < 0) speed = 0;
+        if (speed > maxSpeed) speed = maxSpeed;
+
+        if (Input.GetKey(keyLeft))
+        {
+            gameObject.transform.eulerAngles -= new Vector3(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(keyRight))
+        {
+            gameObject.transform.eulerAngles += new Vector3(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector3(velX * speed, GetComponent<Rigidbody>().velocity.y - 5, velZ * speed);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Gate") == true)
+        {
+            if (inGate == false)
+            {
+                inGate = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Gate") == true)
+        {
+            if (inGate == true)
+            {
+                inGate = false;
+            }
+
+        }
+        if (other.CompareTag("bound") == true && !inGate)
+        {
+            speed = 0;
+        }
+    }
+
+    void CalculateSpeed()
+    {
+        angleY = gameObject.transform.eulerAngles.y * (Mathf.PI / 180);
+        velX = Mathf.Sin(angleY) * Time.deltaTime;
+        velZ = Mathf.Cos(angleY) * Time.deltaTime;
+    }
+}
