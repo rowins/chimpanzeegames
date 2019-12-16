3b5b28e2183162120877229af2c9eebb65941036
t-hoogte <57985731+t-hoogte@users.noreply.github.com>
2019-12-16 15:45:18 +0100

    beweegend object toegevoegd
    scriot is simpel over te kopieren,
    ik heb ook de lichtbronnen verplaatst

---
+++ Assets/Scripts/MovementObstacle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementObstacle : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    bool forward = true;
+    float progress = 0;
+
+    void start()
+    {
+        if (distance < 0) distance = -distance;
+    }
+
+    void Update()
+    {
+        if (forward)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            progress += speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            progress -= speed * Time.deltaTime;
+        }
+
+        if (progress >= distance) forward = false;
+        else if (progress <= 0) forward = true;
+
+    }
+}
