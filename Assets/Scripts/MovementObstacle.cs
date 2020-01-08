0bc9b4ea58be5e2cef5c66c60c0d2f6c49a25f1a
t-hoogte <57985731+t-hoogte@users.noreply.github.com>
2020-01-08 20:35:59 +0100

    bewegwnde autos her-toegevoegd
    her want ze zaten er al in voor de reset maar nu zijn ze beter en ik probeer een ander soort bewegend object toe te voegen maar er gaat nog iets mis

---
+++ Assets/Scripts/MovementObstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementObstacle : MonoBehaviour
+{
+    public float speed;
+    public Vector3 location;
+
+    float progress = 0;
+    Vector3 direction;
+    bool forward = true;
+
+    void start()
+    {
+        //Debug.Log("start");
+        direction = location / location.magnitude;
+    }
+
+    void Update()
+    {
+        if (forward)
+        {
+            transform.position += speed * direction * Time.deltaTime;
+            progress += speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= speed * direction * Time.deltaTime;
+            progress -= speed * Time.deltaTime;
+        }
+
+        if (progress >= location.magnitude) forward = false;
+        else if (progress <= 0) forward = true;
+
+    }
+
+}
